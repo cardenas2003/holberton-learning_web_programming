2e71bcf2f2b444e9959cc0764ec20e988521fbca
cardenas2003 <cardenaschristian2003@gmail.com>
2019-12-06 20:58:18 -0500

    Create 101-function.js

---
+++ 5-javascript/101-function.js
@@ -0,0 +1,10 @@
+var b = myFunction("Hello World!")
+function myFunction(b) {
+  console.log(b)
+}
+
+var a = youFunction(prompt("What happens"))
+function youFunction(a) {
+let c = a*2
+  console.log(c);
+}
